5ea29ed748d5d5ef691160b6b1693773be4c546a
Akane <akane.simpson@gmail.com>
2020-04-08 10:21:45 -0400

    Added routing on index.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App/App'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App/App'
+import './index.scss';
+
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>
+    , 
+    document.getElementById("root"));
