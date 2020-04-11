17f2e4bc3cc0a955f9687a9f0edf602d3b308c7d
Scarlet <developerscarlet@gmail.com>
2020-04-11 00:43:44 -0400

    Create FormElement class export.

--- src/Components/components.tsx
+++ src/Components/components.tsx
@@ -6,7 +6,7 @@ import "./components.scss";
  * 
  * @param props 
  */
-export function Header(props: any){
+function Header(props: any){
     return(
         <header>
             <h1></h1>
@@ -19,100 +19,9 @@ export function Header(props: any){
  * 
  * @param props 
  */
-export function Button(props: any){
+function Button(props: any){
     return(
-        <button name={props.name}></button>
-    )
-}
-
-
-/**
- * 
- * @param props 
- */
-export function Input(props: any){
-    let handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
-        let inputValue = event.target.value;
-    }
-    switch (props.type) {
-        case 'text':
-            return(
-                <input type={props.type} name={props.name} min="" max="" value=""/>
-            );
-        case 'range':
-        case 'number':
-            return(
-                <input type={props.type} name={props.name} min="" max="" step="" value={props.value || null}/>
-            );
-        case 'date':
-        case 'datetime-local':
-        case 'month':
-        case 'week':
-            return(
-                <input type={props.type} name={props.name} min="" max=""/>
-            );
-        case 'tel':
-            return(
-                <input type={props.type} name={props.name} pattern={props.pattern || "[0-9]{3}-[0-9]{3}-[0-9]{4}"}/>
-            );
-        case 'password':
-        case 'radio':
-        case 'checkbox':
-        case 'file':
-        case 'reset':
-        case 'url':
-            default:
-            return(
-                <input type={props.type} name={props.name} value=""/>
-            );
-    }
-}
-
-
-/**
- * 
- * @param props 
- */
-export function Label(props: any){
-    return(
-        <label id={props.id} >
-            {props.message}
-        </label>
-    )
-}
-
-/**
- * 
- * @param props 
- */
-export function TextArea(props: any){
-    return(
-        <textarea></textarea>
-    )
-}
-
-/**
- * 
- * @param props 
- */
-export function SelectList(props: any){
-
-    let handleChange = (event: React.ChangeEvent<HTMLSelectElement>) =>{
-        let selectedValue = event.target.value;
-        props.onSelectChange(selectedValue);
-    }
-
-    let arrayOfData = props.arrayOfData;
-    let options: React.FormEvent<HTMLOptionElement>[] = arrayOfData.map((data: any) =>{
-        <option key={data.id} value={data.value}>
-            {data.name}
-        </option>
-    });
-
-    return(
-        <select id={props.id} onChange={handleChange}>
-            {options}
-        </select>
+        <button name={props.name} onClick={props.method}></button>
     )
 }
 
@@ -120,7 +29,7 @@ export function SelectList(props: any){
  * 
  * @param props
  */
-export function Footer(props: any) {
+function Footer(props: any) {
     return(
         <footer>
             <span>
@@ -128,4 +37,112 @@ export function Footer(props: any) {
             </span>
         </footer>
     )
+}
+
+export class FormElements{
+
+    constructor(){
+
+    }
+    
+    /**
+     * 
+     * @param props
+     * 
+     * @method handleChange - Used 
+     * @property props.id (The id assigned to input)
+     * @property props.type - The type of input being called
+     * @property props.placeholder
+     * @property props.pattern
+     */
+    Input(props: any): any{
+        let handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+            let inputValue = event.target.value;
+        }
+        // @property 
+        switch (props.type) {
+            case 'text':
+                return(
+                    <input type={props.type} id={props.id} minLength={props.min || 1} maxLength={props.max || 100} 
+                    value={props.value || ""} placeholder={props.placeholder || ""}/>
+                );
+            case 'range':
+            case 'number':
+                return(
+                    <input type={props.type} id={props.id} min={props.min || 1} max={props.max || 100} 
+                    step={props.step || 1} value={props.value || ''}/>
+                );
+            case 'date':
+            case 'datetime-local':
+            case 'month':
+            case 'week':
+                return(
+                    <input type={props.type} id={props.id} min={props.main || ""} max={props.max || ""}/>
+                );
+            case 'tel':
+                return(
+                    <input type={props.type} id={props.id} pattern={props.pattern || "[0-9]{3}-[0-9]{3}-[0-9]{4}"}/>
+                );
+            case 'password':
+            case 'radio':
+            case 'checkbox':
+            case 'file':
+            case 'reset':
+            case 'url':
+                default:
+                return(
+                    <input type={props.type} id={props.id} value={props.value || ""}/>
+                );
+        }
+    }
+
+
+    /**
+     * 
+     * @param props 
+     */
+    Label(props: any){
+        return(
+            <label id={props.id}>
+                {props.message}
+            </label>
+        )
+    }
+
+    /**
+     * 
+     * @param props
+     * @property name 
+     */
+    TextArea(props: any){
+        return(
+            <textarea rows={props.row || 4} cols={props.cols || 50} placeholder={props.placeholder || ""}></textarea>
+        )
+    }
+
+    /**
+     * 
+     * @param props 
+     */
+    SelectList(props: any){
+
+        let handleChange = (event: React.ChangeEvent<HTMLSelectElement>) =>{
+            let selectedValue = event.target.value;
+            props.onSelectChange(selectedValue);
+        }
+
+        let arrayOfData = props.arrayOfData;
+        let options: React.FormEvent<HTMLOptionElement>[] = arrayOfData.map((data: any) =>{
+            <option key={data.id} value={data.value}>
+                {data.name}
+            </option>
+        });
+
+        return(
+            <select id={props.id} onChange={handleChange}>
+                {options}
+            </select>
+        )
+    }
+
 }
