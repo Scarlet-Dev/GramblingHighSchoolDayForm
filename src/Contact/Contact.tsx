dc9cf479a82f4861cdbfa1fe7be97e2281223d5a
Scarlet <developerscarlet@gmail.com>
2020-04-11 00:44:22 -0400

    Adding form elements for contact form.

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './Contact.scss';
+import { FormElements } from 'src/Components/components';
 
 export default function Contact(){
+    const fe = new FormElements();
     return(
-        <div></div>
+        <div>
+        <h3></h3>
+        <form action="/api/contact.php" >
+            <div>
+                <fe.Input type="text" placeholder="Enter Full Name"/>
+            </div>
+            <div>
+                <fe.Input type="tel" placeholder="Enter Phone Number"/>
+            </div>
+            <div>
+                <fe.Input type="email"/>
+            </div>
+            <div>
+                <fe.TextArea placeholder="Please enter any questions or comments" />
+            </div>
+        </form>
+        </div>
     )
 }
