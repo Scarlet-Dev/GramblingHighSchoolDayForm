ad38cb961479af526f08ebd9fefef5687ecb1fa7
Scarlet <developerscarlet@gmail.com>
2020-04-11 00:45:10 -0400

    Working on signup form.

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,29 +1,36 @@
 import React from 'react'
-
-// import { Input, Label, SelectList } from "../Components/components";
-// import 'src/static/states';
-
 import './Form.scss'
+import { FormElements } from 'src/Components/components'
+// import 'src/static/states';
 
 
 function FormBody(){
+    const fe = new FormElements();
+    let today = new Date();
+    let maxDate = new Date(today.getUTCFullYear(), today.getMonth(), today.getDate())
     return(
-        <form action="/form.php" method="POST">
+        <form action="/api/form.php" method="POST">
             <section>
                 <div>
-                    <label>Applicant Name: <input id="applicant_name" />
-                    </label>
+                    <fe.Label id="applicant_name_label" message="Applicant Name:">
+                        <fe.Input id="" type="text"/>
+                    </fe.Label>
                 </div>
                 <div>
-                    <label>Applicant's School Name: <input id="applicant_school" />
-                    </label>
+                    <fe.Label id="applicant_school_label" message="Applicant's School Name:">
+                        <fe.Input id="applicant_school" type="text"/>
+                    </fe.Label>
                 </div>
                 <div>
+                    <fe.Label id="applicant_dob_label" message="Applicant's Date of Birth: ">
+                        <fe.Input id="applicant_dob" type="date" max={maxDate}/>
+                    </fe.Label>
                 </div>
-            </section>
-            <section>
-
-                
+                <div>
+                    <fe.Label id="applicant_email" message="">
+                        <fe.Input id="" type="email"/>
+                    </fe.Label>
+                </div>
             </section>
         </form>
     )
