2c577d0a22a5b97984fefe828f9ac7ccec96d76a
Scarlet <developerscarlet@gmail.com>
2020-04-13 00:31:30 -0400

    Added GeneralElements import.

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,37 +1,44 @@
 import React from 'react'
 import './Form.scss'
-import { FormElements } from 'src/Components/components'
+import { FormElements, GeneralElements } from '../Components/components'
 // import 'src/static/states';
 
 
 function FormBody(){
     const fe = new FormElements();
+    const ge = new GeneralElements();
+
     let today = new Date();
     let maxDate = new Date(today.getUTCFullYear(), today.getMonth(), today.getDate())
     return(
         <form action="/api/form.php" method="POST">
-            <section>
                 <div>
-                    <fe.Label id="applicant_name_label" message="Applicant Name:">
-                        <fe.Input id="" type="text"/>
-                    </fe.Label>
+                    <fe.Label id="applicant_name_label" text="Applicant Name:" for="applicant_name"/>
+                    <fe.Input id="applicant_name" type="text"/>
                 </div>
                 <div>
-                    <fe.Label id="applicant_school_label" message="Applicant's School Name:">
-                        <fe.Input id="applicant_school" type="text"/>
-                    </fe.Label>
+                    <fe.Label id="applicant_school_label" text="Applicant's School Name:" for="applicant_school"/>
+                    <fe.Input id="applicant_school" type="text"/>
                 </div>
                 <div>
-                    <fe.Label id="applicant_dob_label" message="Applicant's Date of Birth: ">
-                        <fe.Input id="applicant_dob" type="date" max={maxDate}/>
-                    </fe.Label>
+                    <fe.Label id="applicant_dob_label" text="Applicant's Date of Birth: "  for="applicant_dob"/>
+                    <fe.Input id="applicant_dob" type="date" max={maxDate}/>
                 </div>
                 <div>
-                    <fe.Label id="applicant_email" message="">
-                        <fe.Input id="" type="email"/>
-                    </fe.Label>
+                    <fe.Label id="applicant_email_label" text="Applicant Email" for="applicant_email"/>
+                    <fe.Input id="applicant_email" type="email"/>
                 </div>
-            </section>
+
+                <div>
+                    <span>
+                        <ge.Button id="applicant_cancel" message="Cancel" />
+                    </span>
+                
+                    <span>
+                        <fe.Input id="applicant_submit" message="Submit" type="submit" />
+                    </span>
+                </div>
+            
         </form>
     )
 }
