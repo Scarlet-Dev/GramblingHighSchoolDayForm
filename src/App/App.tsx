c972fa0b514cf11c71b34f9262117c3c45749c78
Scarlet <developerscarlet@gmail.com>
2020-04-16 22:20:47 -0400

    Added more data to AppHome. Stylings included.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -49,8 +49,106 @@ const AppHome = () => {
                 </p>
             </section>
 
-            <section>
+            <section className="carousel">
                 
+            </section>
+            <section className="info">
+                <div className="entry">
+                    <h1>Title 1</h1>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum deleniti soluta, labore reprehenderit iure doloribus aliquid 
+                        consectetur unde dignissimos. Non, nihil hic atque natus impedit libero, voluptatum obcaecati iusto enim eveniet totam pariatur 
+                        magni temporibus adipisci quae, repudiandae unde repellendus eius quaerat? Omnis vero exercitationem autem quidem quo, molestias animi.
+                    </p>
+                </div>
+                <div className="entry">
+                    <h1>Title 2</h1>
+                    <img src={faker.image.cats(200, 200)} alt="Image here"/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae facere quia placeat veritatis consectetur et labore minima. 
+                        Culpa cum velit nihil earum! Non a dolore autem doloribus doloremque! Sequi hic possimus atque explicabo ipsum vitae doloribus alias ad, 
+                        recusandae illum? Fugit quo obcaecati quibusdam omnis culpa inventore totam dolorem rerum expedita! Placeat molestias laudantium, mollitia
+                        veritatis quas iste aspernatur optio labore? Numquam corrupti voluptatem animi debitis aliquid placeat beatae, eaque asperiores 
+                        voluptatum veniam, sint nihil hic facilis libero minima.
+                    </p>
+                </div>
+                <div className="entry">
+                    <h1>Title 2</h1>
+                    <img src={faker.image.cats(200, 200)} alt="Image here"/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae facere quia placeat veritatis consectetur et labore minima. 
+                        Culpa cum velit nihil earum! Non a dolore autem doloribus doloremque! Sequi hic possimus atque explicabo ipsum vitae doloribus alias ad, 
+                        recusandae illum? Fugit quo obcaecati quibusdam omnis culpa inventore totam dolorem rerum expedita! Placeat molestias laudantium, mollitia
+                        veritatis quas iste aspernatur optio labore? Numquam corrupti voluptatem animi debitis aliquid placeat beatae, eaque asperiores 
+                        voluptatum veniam, sint nihil hic facilis libero minima.
+                    </p>
+                </div>
+                <div className="entry">
+                    <h1>Title 2</h1>
+                    <img src={faker.image.cats(200, 200)} alt="Image here"/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae facere quia placeat veritatis consectetur et labore minima. 
+                        Culpa cum velit nihil earum! Non a dolore autem doloribus doloremque! Sequi hic possimus atque explicabo ipsum vitae doloribus alias ad, 
+                        recusandae illum? Fugit quo obcaecati quibusdam omnis culpa inventore totam dolorem rerum expedita! Placeat molestias laudantium, mollitia
+                        veritatis quas iste aspernatur optio labore? Numquam corrupti voluptatem animi debitis aliquid placeat beatae, eaque asperiores 
+                        voluptatum veniam, sint nihil hic facilis libero minima.
+                    </p>
+                </div>
+                <div className="entry">
+                    <h1>Title 3</h1>
+                    <img src={faker.image.cats(200, 200)} alt="Image here"/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae facere quia placeat veritatis consectetur et labore minima. 
+                        Culpa cum velit nihil earum! Non a dolore autem doloribus doloremque! Sequi hic possimus atque explicabo ipsum vitae doloribus alias ad, 
+                        recusandae illum? Fugit quo obcaecati quibusdam omnis culpa inventore totam dolorem rerum expedita! Placeat molestias laudantium, mollitia
+                        veritatis quas iste aspernatur optio labore? Numquam corrupti voluptatem animi debitis aliquid placeat beatae, eaque asperiores 
+                        voluptatum veniam, sint nihil hic facilis libero minima.
+                    </p>
+                </div>
+                <div className="entry">
+                    <h1>Title 4</h1>
+                    <img src={faker.image.cats(200, 200)} alt="Image here"/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae facere quia placeat veritatis consectetur et labore minima. 
+                        Culpa cum velit nihil earum! Non a dolore autem doloribus doloremque! Sequi hic possimus atque explicabo ipsum vitae doloribus alias ad, 
+                        recusandae illum? Fugit quo obcaecati quibusdam omnis culpa inventore totam dolorem rerum expedita! Placeat molestias laudantium, mollitia
+                        veritatis quas iste aspernatur optio labore? Numquam corrupti voluptatem animi debitis aliquid placeat beatae, eaque asperiores 
+                        voluptatum veniam, sint nihil hic facilis libero minima.
+                    </p>
+                </div>
+                <div className="entry">
+                    <h1>Title 5</h1>
+                    <img src={faker.image.cats(200, 200)} alt="Image here"/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae facere quia placeat veritatis consectetur et labore minima. 
+                        Culpa cum velit nihil earum! Non a dolore autem doloribus doloremque! Sequi hic possimus atque explicabo ipsum vitae doloribus alias ad, 
+                        recusandae illum? Fugit quo obcaecati quibusdam omnis culpa inventore totam dolorem rerum expedita! Placeat molestias laudantium, mollitia
+                        veritatis quas iste aspernatur optio labore? Numquam corrupti voluptatem animi debitis aliquid placeat beatae, eaque asperiores 
+                        voluptatum veniam, sint nihil hic facilis libero minima.
+                    </p>
+                </div>
+                <div className="entry">
+                    <h1>Title 6</h1>
+                    <img src={faker.image.cats(200, 200)} alt="Image here"/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae facere quia placeat veritatis consectetur et labore minima. 
+                        Culpa cum velit nihil earum! Non a dolore autem doloribus doloremque! Sequi hic possimus atque explicabo ipsum vitae doloribus alias ad, 
+                        recusandae illum? Fugit quo obcaecati quibusdam omnis culpa inventore totam dolorem rerum expedita! Placeat molestias laudantium, mollitia
+                        veritatis quas iste aspernatur optio labore? Numquam corrupti voluptatem animi debitis aliquid placeat beatae, eaque asperiores 
+                        voluptatum veniam, sint nihil hic facilis libero minima.
+                    </p>
+                </div>
+                <div className="entry">
+                    <h1>Title 7</h1>
+                    <img src={faker.image.cats(200, 200)} alt="Image here"/>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae facere quia placeat veritatis consectetur et labore minima. 
+                        Culpa cum velit nihil earum! Non a dolore autem doloribus doloremque! Sequi hic possimus atque explicabo ipsum vitae doloribus alias ad, 
+                        recusandae illum? Fugit quo obcaecati quibusdam omnis culpa inventore totam dolorem rerum expedita! Placeat molestias laudantium, mollitia
+                        veritatis quas iste aspernatur optio labore? Numquam corrupti voluptatem animi debitis aliquid placeat beatae, eaque asperiores 
+                        voluptatum veniam, sint nihil hic facilis libero minima.
+                    </p>
+                </div>
             </section>
         </div>
     );
@@ -80,7 +178,12 @@ export default function App(){
                     <AppHome />
                 </Route>
             </Switch>
-            <ge.Footer />
+            <ge.Footer className="footer">
+                <hr></hr>
+                <p>
+                    Created by Akane Simpson &copy; 2020
+                </p>
+            </ge.Footer>
         </div>
     )
 }
