0267493cb409834b9a67479fc92e9b59f20ae666
Scarlet <developerscarlet@gmail.com>
2020-04-28 23:30:29 -0400

    Changed ts classes json file.

--- src/static/interests.ts
+++
@@ -1,7 +0,0 @@
-export const interests = [
-    {
-        id: "",
-        name:"",
-        value:""
-    }
-]
--- src/static/programs.ts
+++
@@ -1,7 +0,0 @@
-export const programs = [
-    {
-        id: "",
-        name: "",
-        value: ""
-    },
-]
