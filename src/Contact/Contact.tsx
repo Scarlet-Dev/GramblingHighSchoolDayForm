c002acf677610a6823fdbf72a35750c3e99d67e9
Scarlet <developerscarlet@gmail.com>
2020-04-29 21:20:26 -0400

    Added dropdown to contact form.

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -7,6 +7,31 @@ const submitForm = () => {
 
 }
 
+const Dropdown = (props: any) => {
+
+    let handleChange = (event: React.ChangeEvent<HTMLSelectElement>) =>{
+        let selectedValue = event.target.value;
+        props.onSelectChange(selectedValue);
+    }
+
+    let arrayOfData = props.data;
+    let options= arrayOfData.map((obj: any) =>
+        {
+            return(
+                <option key={obj.id} value={obj.value}>
+                    {obj.id}
+                </option>
+                )
+        });
+
+    return(
+        <select id={props.id} className="form-group dropdown" onChange={handleChange}>
+            <option>Select a value</option>
+            {options}
+        </select>
+    );
+}
+
 export default function Contact(){
     return(
         <div>
@@ -22,7 +47,7 @@ export default function Contact(){
                 <input type="email" placeholder="Enter Email Address"/>
             </div>
             <div>
-                
+                <Dropdown data={programs}/>
             </div>
             <div>
                 <textarea placeholder="Please enter any questions or comments" cols={100} rows={5}></textarea>
