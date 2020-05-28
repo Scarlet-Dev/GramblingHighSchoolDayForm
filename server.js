8687453fa5f1cbbc69496e2b0007d289fba52c8b
Scarlet <developerscarlet@gmail.com>
2020-05-27 20:59:21 -0400

    Added db and mail service. Added more api endpoint

--- server.js
+++ server.js
@@ -1,43 +1,81 @@
 const express = require('express');
-const fs = require('fs');
-const sqlite = require('sql.js');
+const cors = require('cors');
+const mailService = require('./services/email');
+const dbService = require('./services/database')
 
-// const filebuffer = fs.readFileSync("db/usda.sqlite3");
-
-// const db = new sqlite.Database(filebuffer);
-await initSqlJs({
-    locateFile: file => ""
-}).then(function(SQL){
-    var db = new SQL.Database();
-});
-
-// const db = new SQL.Database();
-
+// Spinning up settings
+//
+//
+/////////////////////////////
 const app = express();
-
 app.set("port", process.env.PORT || 3001);
-
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
 }
 
-const COLUMNS = [
-    "",
-    "",
-    ""
-]
+// Services and middleware
+//
+//
+////////////////////////////
+mailService.default(app);
+const corsOptions = {
+    origin: process.env.ORIGIN | '*',
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
-// app.get("/api/getStates", (req, res) => {
-//     const param = req.query.q;
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', `*`);
+    res.setHeader('Access-Control-Allow-Headers', `*`);
+    next();
+})
+
+// API endpoints.
+//
+//
+///////////////////
+app.get("/api/getStates", (req, res) => {
+    // const param = req.query.q;
     
-//     if(!param){
-//         res.json({
-//             error: ""
-//         })
-//         return;
-//     }
-// })
+    // if(!param){
+    //     res.json({
+    //         error: ""
+    //     })
+    //     return;
+    // }
+})
 
+// Returns a list of programs.
+app.get("/api/getPrograms", (req, res) =>{
+
+})
+
+// Returns a list of applicants.
+app.get("/api/getApplicants", (req, res) => {
+    let body = {};
+    res.send(body)
+});
+
+// Returns applicant information via applicant Id.
+app.get("/api/getApplicant/:id", (req, res) => {
+
+});
+
+// Processes the applicant's info. Saves it to the db.
+app.post("/api/registrationForm", (req, res) => {
+    
+});
+
+// Processes the contact form and sends a email to admin.
+app.post("/api/saveContactForm", (req, res) => {
+
+});
+
+
+// Init server
+//
+//
+//////////////////
 app.listen(app.get("port", () => {
     console.log("Listening right now.")
 }))
