21131c6d33569db3647ee4cd21ea08e6645687d7
Scarlet <developerscarlet@gmail.com>
2020-05-29 23:46:48 -0400

    Added bodyParser.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+
 const mailService = require('./services/email');
 const dbService = require('./services/database')
 
