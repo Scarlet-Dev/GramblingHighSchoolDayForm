f4626f2046e66e14a481dfa02f40925fdfd37227
Scarlet <developerscarlet@gmail.com>
2020-06-04 00:03:09 -0400

    Convert func component into class component., Add interface IContactForm

--- client/src/Contact/Contact.tsx
+++ client/src/Contact/Contact.tsx
@@ -27,30 +27,43 @@ const Dropdown = (props: any) => {
     );
 }
 
-export default function Contact(){
-    return(
-        <div>
-        <h3></h3>
-        <form onSubmit={submitForm} >
+interface IContactForm{
+    programs: []
+}
+
+class ContactForm extends React.Component<any, IContactForm> {
+    constructor(props:any){
+        super(props);
+        this.state= {
+            programs:[]
+        }
+    }
+    render(){
+
+        return(
             <div>
-                <input type="text" placeholder="Enter Full Name"/>
+                <h3></h3>
+                <form onSubmit={this.onFormSubmit} >
+                    <div>
+                        <input type="text" placeholder="Enter Full Name"/>
+                    </div>
+                    <div>
+                        <input type="tel" placeholder="Enter Phone Number"/>
+                    </div>
+                    <div>
+                        <input type="email" placeholder="Enter Email Address"/>
+                    </div>
+                    <div>
+                        <Dropdown data={this.state.programs}/>
+                    </div>
+                    <div>
+                        <textarea placeholder="Please enter any questions or comments" cols={100} rows={5}></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" name="Submit" />
+                    </div>
+                </form>
             </div>
-            <div>
-                <input type="tel" placeholder="Enter Phone Number"/>
-            </div>
-            <div>
-                <input type="email" placeholder="Enter Email Address"/>
-            </div>
-            <div>
-                <Dropdown data={programs}/>
-            </div>
-            <div>
-                <textarea placeholder="Please enter any questions or comments" cols={100} rows={5}></textarea>
-            </div>
-            <div>
-                <input type="submit" name="Submit" />
-            </div>
-        </form>
-        </div>
-    )
+        )
+    }
 }
